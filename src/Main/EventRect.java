f78df479335acd44f7d8590d9851f6252232e4a5
FanchonSora <khanhngan1491@gmail.com>
2024-06-02 19:41:52 +0700

    Check

--- src/Main/EventRect.java
+++ src/Main/EventRect.java
@@ -1,9 +1,6 @@
 package Main;
-
 import java.awt.Rectangle;
-
-public class EventRect extends Rectangle{
-
+public class EventRect extends Rectangle {
     int eventRectDefaultX, eventRectDefaultY;
     boolean eventDone = false;
     
