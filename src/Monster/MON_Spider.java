520cb6587f2af682cd67d459b826a324166061ae
Hoi dan it <khanhngan1491@gmail.com>
2024-06-15 00:27:58 +0700

    Fix bug

--- src/Monster/MON_Spider.java
+++ src/Monster/MON_Spider.java
@@ -39,10 +39,10 @@ public class MON_Spider extends Entity implements MonsterInt<Graphics2D, GamePan
     }
     public void getImage(){
         
-        up1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 86);
-        down1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 86);
-        left1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 86);
-        right1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 86);
+        up1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 100);
+        down1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 100);
+        left1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 100);
+        right1 = setupMonsterImages("Monster/SPider/Spider",gamePanel.tileSize + 100,gamePanel.tileSize + 100);
     }   
     @Override
     public void setAction(){}
@@ -67,7 +67,7 @@ public class MON_Spider extends Entity implements MonsterInt<Graphics2D, GamePan
         int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
     
         if(Defeat == false) {
-            g2.drawImage(image, screenX, screenY, gamePanel.tileSize + 100, gamePanel.tileSize + 100, null);
+            g2.drawImage(image, screenX, screenY, gamePanel.tileSize + 130, gamePanel.tileSize + 100, null);
         }
     }
     
